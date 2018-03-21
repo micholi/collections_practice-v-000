a4a12e593bd079681a9e78d3201733fe4f2910fc
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 22:09:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,8 +22,9 @@ def swap_elements(array)
 # array
   array.each_with_index do |element, index|
     if index == 1
+      placeholder = array[index]
       array[index] = array[index + 1]
-      placeholder = element
+    
     elsif index == 2
       array[index] = placeholder
 end
