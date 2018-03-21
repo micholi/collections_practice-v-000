6ffeb1c42441236e2693b7b922b8d8f6347d0c2a
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 22:06:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -22,13 +22,13 @@ def swap_elements(array)
 # array
   array.each_with_index do |element, index|
     if index == 1
-      array[index], array[index + 1] = array[index], array[index + 1]
+      array[index], array[index + 1] = array[index + 1], array[index]
 
 end
 array
 
   
-
+end
 end
 
 def reverse_array(integers)
