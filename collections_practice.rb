79d1c024e8d5dd42d64de4bf44d6d2be0ad25bb4
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 21:05:01 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,7 +44,7 @@ def sum_array(integers)
 end
 
 def add_s(words)
-  words.each_with_index.collect{ |word, index| num = index }
-  num == 1 ? word : "#{word}s"
+  words.each_with_index.collect{ |word, index| index == 1 ? word : "#{word}s" }
+
 
 end
