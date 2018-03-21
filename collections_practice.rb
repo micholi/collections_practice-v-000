03ee053078a76192ba8713b54b68470595c8eed8
Michelle Olivieri <michelleo72@gmail.com>
2018-03-21 21:50:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,18 +20,11 @@ def swap_elements(array)
   # this can't be right
   # array[1], array [2] = array[2], array[1]
 # array
-  swapped_array = []
   array.each_with_index do |element, index|
     if index == 1
-      store_element_one = element
-        new_val = array[2]
-        swapped_array << new_val
-      elsif index == 2
-        new_val_two = store_element_one
-          swapped_array << new_val_two
-        else swapped_array << element
+      element = array[index + 1]
 end
-swapped_array
+array
     end
 
 end
